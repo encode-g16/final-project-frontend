aa07d030175aa34b6b22fa3a32555cc4286eb139
Francis_ldn <fnhh04@gmail.com>
2022-07-29 22:54:06 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CreateEvent from './pages/CreateEvent';
+import Event from './pages/Event';
+import Home from './pages/Home'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/create-event">
+            <CreateEvent />
+          </Route>
+          <Route path="/events/:id">
+            <Event />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/EventBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventBox = () => {
+  return (
+    <div>EventBox</div>
+  )
+}
+
+export default EventBox
