e9b673bcdb9b2ac2963816e192433cb41e53533d
ben <phas0ruk@gmail.com>
2022-08-04 13:35:48 -0700

    change to create event form

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 export default function CreateEventForm() {
     return (
-        <div className="w-1/2 h-1/2 border border-gray-300 rounded-md shadow-md mx-auto">
+        <div className="w-1/2 h-1/2 border border-gray-300  bg-white rounded-md shadow-md mx-auto">
             <p className="font-bold text-xl text-center py-3 px-1">Create Your Event</p>
             <hr />
             <div className="md:grid md:grid-cols-4 md:gap-4">
@@ -37,12 +38,10 @@ export default function CreateEventForm() {
                     md:col-span-3
                 "/>
    
-                <input type="checkbox" id="event-checkbox" name="event-checkbox" value="1" className="block mx-auto mt-4 md:col-span-1 rounded border border-gray-500"/>
+                <input type="checkbox" id="event-checkbox" name="event-checkbox" value="1" className="block ml-2 mt-4 md:mx-auto md:col-span-1 rounded border border-gray-500"/>
                 <label htmlFor="checkbox" className="mx-2 mt-2 md:col-span-3">I agree to pay gas fees in Eth for the creation of the event and that it is non-refundable</label>
 
-                <button type="submit" className="block col-span-4 p-2 m-2 bg-blue-600 text-center text-lg rounded-md text-white font-semibold hover:bg-blue-700">Create Event</button>
-            
-            
+                <button type="submit" className="block p-2 mx-auto md:m-2 md:col-span-4  bg-blue-600 text-center text-lg rounded-md text-white font-semibold hover:bg-blue-700">Create Event</button>
             </div>
         </div>
 
