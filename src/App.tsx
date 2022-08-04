cf4e751ac2af587f7a4aa68ebcdc49b146a1b2d6
ben <phas0ruk@gmail.com>
2022-08-04 16:18:03 -0700

    updates create-event route, deletes old event form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import CreateEvent from './pages/CreateEvent';
 import Event from './pages/Event';
 import Home from './pages/Home'
-import CreateEventNew from './pages/CreateEventNew';
+import CreateEvent from './pages/CreateEvent'
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-
 
 export default function App() {
   return (
@@ -17,9 +15,6 @@ export default function App() {
         <Switch>
           <Route exact path="/create-event">
             <CreateEvent />
-          </Route>
-          <Route exact path="/create-event-new">
-            <CreateEventNew />
           </Route>
           <Route path="/events/:id">
             <Event />
