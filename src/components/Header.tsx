2c8de6029f72e316830ec7c8dc9db928d92d884a
Francis_ldn <fnhh04@gmail.com>
2022-08-08 06:34:14 +0200

    add responsive styling to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className="flex justify-between mx-auto h-[200px]">
-      <div className="w-[20rem] h-[6rem] my-10 ml-12">
+    <div className="flex justify-between mx-auto gap-5 h-[200px]">
+      <div className="w-100 xl:w-[20rem] h-[6rem] my-10 ml-8 xl:ml-12">
           <Link to={"/"}><img src="/blocktic_logo.png" alt="logo" className="h-full w-full" /></Link>
       </div>
       <div className="mr-[10%] my-auto">
-        <ul className="flex gap-14 text-2xl font-bold">
+        <ul className="flex gap-5 xl:gap-14 text-lg xl:text-2xl font-bold">
           <li className="cursor-pointer menu-items"><Link to={"/create-event"}>Create Event</Link></li>
           <li className="cursor-pointer menu-items"><Link to={"/my-created-events"}>My Created Events</Link>  </li>
           <li className="cursor-pointer menu-items"><Link to={"/all-events"}>All Events</Link></li>
